30eab4b4eb9903d47be16296dc91fa5ed674b106
Sebastian Sipos <sebastiansipos90@gmail.com>
2019-04-23 00:50:02 +0300

    WIP on install

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir .tmp
+cd .tmp
+
+if [ ! -d plugins ]; then
+    echo "TPM already installed"
+else
+    wget https://github.com/tmux-plugins/tpm/archive/master.zip -o tpm.zip
+    unzip tpm.zip -d ../plugins/tmp
+fi
+
