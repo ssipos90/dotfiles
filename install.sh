9910931a20f4a9035a1531fa84a605f5992e2a94
Sebastian Sipos <sebastiansipos90@gmail.com>
2019-06-21 00:08:56 +0300

    upgrade installer

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-set -eu
+set -u
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
+declare -a dependencies=("xdotool" "git" "zsh" "tmux" "vim")
+echo "checking dependencies"
+for dep in "${!dependencies[@]}"; do
+  dependency=${dependencies[$dep]}
+  command -v ${dependency} > /dev/null
+  if [[ $? -ne 0 ]]; then
+    echo "Dependency missing: ${dependency}" >&2
+    exit 1
+  fi
+done
+
 cd $SCRIPTPATH
 
+set -e
+echo "loading git submodules"
 git submodule update --init --recursive
 
+echo "adding paths to path"
 grep -Fxq "source $SCRIPTPATH/paths" $HOME/.profile || echo -e "source $SCRIPTPATH/paths\n\n$(cat $HOME/.profile)" > $HOME/.profile
 
 declare -A symlinks=(
@@ -17,19 +31,20 @@ declare -A symlinks=(
   ["vim"]=".vim"
   ["npmrc"]=".npmrc"
 )
-
+echo "creating dotfile symlinks"
 for symlink in "${!symlinks[@]}"; do
     SYMLINK=$HOME/${symlinks[$symlink]}
     if [[ -e $SYMLINK ]]; then
-        echo "Symlink already exists: $SYMLINK"
+        echo "Symlink already exists: $SYMLINK" >&2
     else
         ln -s $SCRIPTPATH/$symlink $SYMLINK 
     fi
 done
 
 declare -a installers=("vim" "tmux")
-
+echo "running sub installers"
 for installer in "${installers[@]}"; do
-    $installer/install.sh
+    echo "running installer for $installer"
+    $installer/install.sh | sed -e 's/^/ > /;'
 done
 
