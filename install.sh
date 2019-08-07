939a055e531776abdd7c9f3d7c89be69639ed87d
Sebastian Sipos <sebastian.sipos@uvision.ro>
2019-08-07 11:38:16 +0300

    Fix install script, ffs.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 mkdir .tmp
 cd .tmp
 
-if [ ! -d plugins ]; then
+if [[ -d plugins ]]; then
     echo "TPM already installed"
 else
     wget https://github.com/tmux-plugins/tpm/archive/master.zip -o tpm.zip
