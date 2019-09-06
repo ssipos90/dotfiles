20b22dab756085c6e35c6ad41f062baa3d0f26df
Sebastian Sipos <sebastiansipos90@gmail.com>
2019-09-06 13:07:11 +0300

    fuck this installer

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+set -eux
 
-mkdir .tmp
-cd .tmp
+rm -rf .temp
+mkdir .temp
+
+TPM_VERSION=3.0.0
+TPM_DOWNLOAD_VERSION=v${TPM_VERSION}
+TPM_ARCHIVE_DIR=tpm-${TPM_VERSION}
 
 if [[ -d plugins ]]; then
     echo "TPM already installed"
-else
-    wget https://github.com/tmux-plugins/tpm/archive/master.zip -o tpm.zip
-    unzip tpm.zip -d ../plugins/tmp
+    exit 1
 fi
+
+mkdir plugins
+git clone https://github.com/tmux-plugins/tpm plugins/tpm
+exec sh plugins/tpm/tpm
 
