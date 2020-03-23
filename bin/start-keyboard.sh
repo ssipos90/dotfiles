c1d09f7be7c06605750ab0e2b14034ba7eb87114
Sebastian Sipos <sebastiansipos90@gmail.com>
2020-03-23 13:37:11 +0200

    fix startup script

--- bin/start-keyboard.sh
+++ bin/start-keyboard.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 xkbuserdir=$HOME/.dotfiles/confs/xkb
-if [ -d $xkbuserdir ]; then
-    xkbcomp -synch -w3 -I$xkbuserdir $xkbuserdir/keymap/custom.xkb $DISPLAY
+if [[ -d $xkbuserdir ]]; then
+    xkbcomp -synch -w3 -I$xkbuserdir $xkbuserdir/keymap/custom.xkb $DISPLAY > /dev/null
 fi
 
-if [[ $(lsusb -d 046d:c336 > /dev/null) -eq 0 ]]; then
+
+if lsusb -d 046d:c336 > /dev/null ; then
     g810-led -r 1 00ff00 &
     g810-led -r 2 00ff00 &
     g810-led -r 3 00ff00 &
