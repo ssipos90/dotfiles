45fbf8c4a144016f60c67631e00ae0283936127c
Sebastian Sipos <sebastiansipos90@gmail.com>
2020-04-02 11:11:44 +0300

    Update keyboard. Fix i3 spinner. Ignore empty password

--- bin/start-keyboard.sh
+++ bin/start-keyboard.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# set capslock off
+if xset q | grep "Caps Lock:   on"; then
+  xdotool key Caps_Lock
+fi
+
+# load custom xkb symbols
 xkbuserdir=$HOME/.dotfiles/confs/xkb
 if [[ -d $xkbuserdir ]]; then
     xkbcomp -synch -w3 -I$xkbuserdir $xkbuserdir/keymap/custom.xkb $DISPLAY > /dev/null
 fi
 
+# restart xcape
+systemctl --user restart xcape.service
 
+# load Logitech keyboard LED profie
 if lsusb -d 046d:c336 > /dev/null ; then
     g810-led -r 1 00ff00 &
     g810-led -r 2 00ff00 &
@@ -13,5 +22,4 @@ if lsusb -d 046d:c336 > /dev/null ; then
     g810-led -r 4 ff0000 &
     g810-led -r 5 ff0000 &
 fi
-
 
