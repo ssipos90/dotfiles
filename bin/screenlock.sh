6dc1ef885f8a0942e4d4c6dd30e994a856b139d9
Sebastian Sipos <sebastiansipos90@gmail.com>
2020-07-17 13:46:46 +0300

    Upgrade screenlock

--- bin/screenlock.sh
+++ bin/screenlock.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -eu
 
-SCREENSHOT=$(mktemp --suffix=.png)
-
-import -window root jpeg:- | convert - -scale 20% -blur 0x2.5 -resize 500% $SCREENSHOT
+RESOLUTION=$(xrandr -q | sed -n 's/.*current[ ]\([0-9]*\) x \([0-9]*\),.*/\1x\2/p')
 
 # lock the screen
-exec i3lock -n -i $SCREENSHOT -e $@
-
+import -window root jpeg:- \
+    | convert - -scale 20% -blur 0x2 -resize 500% RGB:- \
+    | exec i3lock -n --raw $RESOLUTION:rgb -i /dev/stdin -e $@
