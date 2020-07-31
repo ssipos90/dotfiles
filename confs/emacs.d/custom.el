a2254e49d68daf4b0ac0cdb04b1c1b73a819a75b
Sebastian Sipos <sebastiansipos90@gmail.com>
2020-07-31 19:03:55 +0300

    fix clipboard madness
    extract custom shit to the custom.el file

---
+++ confs/emacs.d/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("450f3382907de50be905ae8a242ecede05ea9b858a8ed3cc8d1fbdf2d57090af" "dd4628d6c2d1f84ad7908c859797b24cc6239dfe7d71b3363ccdd2b88963f336" "f66abed5139c808607639e5a5a3b5b50b9db91febeae06f11484a15a92bde442" default)))
+ '(package-selected-packages
+   (quote
+    (dockerfile-mode ## twilight-theme evil-terminal-cursor-changer js-mode pug-mode indium vue-mode js2-mode evil-magit magit use-package helm evil-visual-mark-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
